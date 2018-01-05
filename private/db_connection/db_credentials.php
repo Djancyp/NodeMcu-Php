2986371d3a82484bb630be903bd1f504dcf642c7
Djan <youcyprus@gmail.com>
2018-01-05 12:58:43 +0000

    Update db_credentials.php

--- private/db_connection/db_credentials.php
+++ private/db_connection/db_credentials.php
@@ -2,7 +2,7 @@
 
 define("DB_SERVER", "localhost");
 define("DB_USER", "root");//Database User
-define("DB_PASS", "canramadan");//Database Password
-define("DB_NAME", "webuser");//Database Name
+define("DB_PASS", "password");//Database Password
+define("DB_NAME", "database");//Database Name
 
 ?>
